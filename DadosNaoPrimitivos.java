a46c36fd993cefd86671b5228ac06446e17f3c85
Alyson Marques <129118650+MarquesDevADS@users.noreply.github.com>
2025-01-19 14:57:31 -0300

    Add files via upload

---
+++ DadosNaoPrimitivos.java
@@ -0,0 +1,24 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) { /* Creating a ninja and testing methods */
+
+        /* name of ninja A */
+        String nome = "Alyson";
+        String nomeNinjaAUpperCase = nome.toUpperCase();
+        System.out.println("Nome do ninja A: "+nomeNinjaAUpperCase);
+
+        /* name of ninja B */
+        String nomeNinjaB = "Bruno";
+        String nomeNinjaBUpperCase = nomeNinjaB.toUpperCase();
+        System.out.println("Nome do ninja B: "+nomeNinjaBUpperCase);
+
+        /* village they lives */
+        String aldeiaPertencente = "Aldeia da Nevoa";
+        String aldeiaPertencenteUpperCase = aldeiaPertencente.toUpperCase();
+        System.out.println("Aldeia onde vivem os ninjas: "+aldeiaPertencenteUpperCase);
+
+
+
+    }
+}
---
+++ DadosPrimitivos.java
@@ -0,0 +1,11 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+       int Idade = 19;
+       double Altura = 1.68;
+       char Inicial = 'A';
+       boolean VivoOuMorto = true;
+       Long SaldoBancario = 999999L;
+    }
+}
