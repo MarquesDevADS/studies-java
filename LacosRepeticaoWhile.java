4a8e3362b79d20dc2b80835422f7193f48154876
Alyson Marques <129118650+MarquesDevADS@users.noreply.github.com>
2025-01-22 02:00:43 -0300

    Add files via upload

---
+++ LacosRepeticaoWhile.java
@@ -0,0 +1,14 @@
+package Condicoes;
+
+public class LacosRepeticaoWhile {
+    public static void main(String[] args) {
+
+        int numeroClones = 0;
+        int numeroMaximo = 5;
+
+        while (numeroClones < numeroMaximo) {
+            numeroClones++;
+            System.out.println("kage bunshin no jutsu, clone número " + numeroClones);
+        }
+    }
+}
